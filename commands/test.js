3b3205703a757ad6e983ed2a848d0fb4b93a3deb
Marti Comas <m4rtiman21@protonmail.com>
2023-11-20 23:59:38 +0100

    balls moment

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,7 @@ import command from "../helpers/command.js"
 export default {
     data: command("test", "Test if the bot is running"),
     async execute(interaction){
+        //balls
         return interaction.reply({content: "heloo wowo"})
     }
 }
